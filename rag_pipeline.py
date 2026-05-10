07ccec9f6cf96b0d1a225480553256f25707483e
williamthesenate <williamp.jackson@calbaptist.edu>
2026-05-09 18:31:42 -0700

    Connect to VoyageAI, generate embeddings and store in 'embeddings.json'.

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -3,13 +3,33 @@ import os
 from dotenv import load_dotenv
 import json
 from pypdf import PdfReader
+import voyageai
+import time
+
+load_dotenv()
+
+claude_client=anthropic.Anthropic(api_key=os.getenv("ANTHROPIC_API_KEY"))
+voyage_client = voyageai.Client(api_key=os.getenv("VOYAGE_API_KEY"))
 
 reader = PdfReader("algebra-and-trigonometry-2e_-_WEB.pdf")
 raw_text = []
 for i in range(140, 154):
     page_text = reader.pages[i].extract_text()
     raw_text.append(page_text)
+    
+def generate_embedding(text):
+    embedding = voyage_client.embed([text], model="voyage-3")
+    return embedding.embeddings[0]
 
-for i in range(0, 4):
-    print(raw_text[i])
-    print(len(raw_text[i]))
+embeddings = []
+for i in range(len(raw_text)):
+    embedding = generate_embedding(raw_text[i])
+    chunk = {
+        "text": raw_text[i],
+        "embedding": embedding
+    }
+    embeddings.append(chunk)
+    # time.sleep(20)
+    
+with open("embeddings.json", "w") as f:
+    json.dump(embeddings, f, indent=4)
